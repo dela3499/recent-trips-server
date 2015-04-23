f1b0c9d708f34aa69e112af18ac3d9faa79d0b3c
cdelaguardia <dela3499@gmail.com>
2015-04-22 21:31:48 -0700

    Basic test on add_trip

--- redis_interface.py
+++ redis_interface.py
@@ -1,5 +1,9 @@
 import redis
+import numpy.random as npr
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+def p(key): 
+	print r.zrange(key,0,-1)
 
 def trim_to_highest(key,n):
 	""" Trim sorted set to n elements, removing elements 
@@ -11,3 +15,19 @@ def add_trip(user_id, trip_data, trip_start_timestamp, size_limit = 10):
 		if set length exceeds size_limit."""
 	r.zadd(user_id, trip_start_timestamp, trip_data)
 	trim_to_highest(user_id, size_limit)
+
+n_users = 100
+n_trips = 1000
+
+user_ids = [str(int(xi * 10000)) for xi in npr.random(n_users)]
+#print user_ids
+trip_user_ids = [npr.choice(user_ids) for _ in range(n_trips)]
+trip_datas = [str(xi) for xi in npr.random(n_trips)]
+trip_start_times = [int(xi) for xi in npr.random(n_trips)]
+
+for user_id, trip_data, trip_start_time in zip(trip_user_ids, trip_datas, trip_start_times):
+	add_trip(user_id, trip_data, trip_start_time)
+
+for user_id in user_ids[:10]:
+	#p(user_id)
+	print r.zcard(user_id)
